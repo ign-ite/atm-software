74c92c97bc27e6e8d8beb928a83ded90860b3965
Varun Kumar <varuntheace@gmail.com>
2024-06-23 11:09:03 +0530

    first

---
+++ at.py
@@ -0,0 +1,55 @@
+import getpass
+import string
+import os
+
+
+users = ['user', 'user2', 'user3']
+pins = ['1234', '2222', '3333']
+amounts = [1000, 2000, 3000]
+count = 0
+while True:
+	user = input('\nENTER USER NAME: ')
+	user = user.lower()
+	if user in users:
+		if user == users[0]:
+			n = 0
+		elif user == users[1]:
+			n = 1
+		else:
+			n = 2
+		break
+	else:
+		print('----------------')
+		print('****************')
+		print('INVALID USERNAME')
+		print('****************')
+		print('----------------')
+
+
+while count < 3:
+	print('------------------')
+	print('******************')
+	pin = str(getpass.getpass('PLEASE ENTER PIN: '))
+	print('******************')
+	print('------------------')
+	if pin.isdigit():
+		if user == 'user1':
+			if pin == pins[0]:
+				break
+			else:
+				count += 1
+				print('-----------')
+				print('***********')
+				print('INVALID PIN')
+				print('***********')
+				print('-----------')
+				print()
+
+		if user == 'user2':
+			if pin == pins[1]:
+				break
+			else:
+				count += 1
+				print('-----------')
+				print('***********')
+				print('INVALID PIN')
